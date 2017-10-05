205f714229be734a155bca30fa09e03cc97aa080
Alexey Paraschevin <paraschevin.alexei@gmail.com>
2017-10-05 23:34:46 +0300

    Almost done

--- Filling/Form1.cs
+++ Filling/Form1.cs
@@ -11,46 +11,62 @@ using System.IO;
 
 namespace Filling
 {
-    public class BPstruct
-    {
-        public int x, y, flag;
-
-        public BPstruct()
-        {
-            x = 0;
-            y = 0;
-            flag = 0;
-        }
-
-        public BPstruct(int x, int y, int flag)
-        {
-            this.x = x;
-            this.y = y;
-            this.flag = flag;
-        }
-
-        public int Compare(BPstruct arg2)
-        {
-            BPstruct arg1 = this;
-            int i = arg1.y - arg2.y;
-            if (i != 0)
-                return ((i < 0) ? -1 : 1);
-            i = arg1.x - arg2.x;
-            if (i != 0)
-                return ((i < 0) ? -1 : 1);
-
-            i = arg1.flag - arg2.flag;
-            return ((i < 0) ? -1 : 1);
-        }
-    }
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            BP = new List<BPstruct>();
+            
             
         }
+        public class BPstruct
+        {
+            public int x, y, flag;
+
+            public BPstruct()
+            {
+                x = 0;
+                y = 0;
+                flag = 0;
+            }
+
+            public BPstruct(int x, int y, int flag)
+            {
+                this.x = x;
+                this.y = y;
+                this.flag = flag;
+            }
+
+            public int Compare(BPstruct arg2)
+            {
+                BPstruct arg1 = this;
+                int i = arg1.y - arg2.y;
+                if (i != 0)
+                    return ((i < 0) ? -1 : 1);
+                i = arg1.x - arg2.x;
+                if (i != 0)
+                    return ((i < 0) ? -1 : 1);
+
+                i = arg1.flag - arg2.flag;
+                return ((i < 0) ? -1 : 1);
+            }
+        }
+
+        public class MyComparer : IEqualityComparer<BPstruct>
+        {
+            public bool Equals(BPstruct x1, BPstruct x2)
+            {
+                return (x1.x == x2.x) && (x1.y == x2.y) && (x1.flag == x2.flag);
+            }
+
+            public int GetHashCode(BPstruct obj)
+            {
+                return obj.x ^ obj.y ^ obj.flag;
+            }
+
+        }
+
         private bool chosing = false;
         private bool loaded = false;
         private Point chosenPoint;
@@ -59,11 +75,11 @@ namespace Filling
         private bool pointIsChosen = false;
 
         private static List<BPstruct> BP;
-        private static int BPstart = 0;
-        private static int BPend = 0;
+        private static int BPstart;
+        private static int BPend;
         private static Color BorderValue;
         private static int BLOCKED = 1;
-        private static int UNBLOCKED = 0;
+        private static int UNBLOCKED = 2;
 
         private void select_image_button_Click(object sender, EventArgs e)
         {
@@ -84,6 +100,7 @@ namespace Filling
         {
             if (!loaded)
                 return;
+            pictureBox1.Image = selectedImage;
             pictureBox1.Cursor = Cursors.Cross;
             chosing = true;
         }
@@ -117,39 +134,54 @@ namespace Filling
         {
             if (!pointIsChosen)
                 return;
-            
+            BP = new List<BPstruct>();
+            BPend = 0;
+            BPstart = 0;
+            CurrentPixel = new BPstruct();
+
 
             img = new Bitmap(selectedImage);
-            BorderValue = img.GetPixel(chosenPoint.X, chosenPoint.Y);
+            BorderValue = Color.Black;//img.GetPixel(chosenPoint.X, chosenPoint.Y);
             BorderFill(chosenPoint.X, chosenPoint.Y);
             pictureBox1.Image = img;
 
             MessageBox.Show("Border selected", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information); 
         }
 
-        
-
-        
-        
-
-
-        private static void BorderFill(int x, int y)
+        private void refresh()
         {
-            do
-            {
-                TraceBorder(x, y);
-                SortBP();
-                ScanRegion(ref x, ref y);
-            }
-            while (BPstart < BPend);
+            pictureBox1.Image = img;
+        }
+
+        
+
+        
+        
+
+
+        private  void BorderFill(int x, int y)
+        {
+
+           
+            TraceBorder(x, y);
+            SortBP();
+            ScanRegion(ref x, ref y);
+
+            TraceBorder(x, y);
+            SortBP();
+            ScanRegion(ref x, ref y);
+
+
             FillRegion();
+            
+            
         }
 
         private static void ScanRegion(ref int x, ref int y)
         {
             int i = BPstart;
             int xr;
-            while (i < BPend)
+            while (i < BPend - 1)
             {
                 if (BP[i].flag == BLOCKED)
                     ++i;
@@ -179,36 +211,45 @@ namespace Filling
             BP.Sort(delegate (BPstruct s1, BPstruct s2) { return s1.Compare(s2); });
         }
 
-        private static void FillRegion()
+        private  void FillRegion()
         {
-            for (int i = 0; i<BPend; )
+            var BP1 = BP.Distinct(new MyComparer()).ToList();
+            int it = 0;
+            for (int i = 0; i<BP1.Count - 1; )
             {
-                if (BP[i].flag == BLOCKED)
+                if (BP1[i].flag == BLOCKED)
                     ++i;
                 else
-                if (BP[i].y != BP[i + 1].y)
+                if (BP1[i].y != BP1[i + 1].y)
                     ++i;
                 else
                 {
-                    if (BP[i].x < BP[i+1].x - 1)
+                    if (BP1[i].x < BP1[i+1].x - 1)
                     {
                         Graphics g = Graphics.FromImage(img);
-                        g.DrawLine(Pens.Red, new Point(BP[i].x + 1, BP[i].y), new Point(BP[i + 1].x - 1, BP[i + 1].y));
-                        
+                        g.DrawLine(Pens.Red, new Point(BP1[i].x + 1, BP1[i].y), new Point(BP1[i + 1].x - 1, BP1[i + 1].y));
+                        refresh();
+
                         i += 2;
                     }
+                }
+                ++it;
+                if (it >= BPend*100)
+                {
+                    //MessageBox.Show("Some error", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
                 }
             }
         }
 
-        private static BPstruct CurrentPixel = new BPstruct();
+        private static BPstruct CurrentPixel;
         private static int d = 0;
         private static int PrevD = 0;
         private static int PrevV = 0;
 
         private static void TraceBorder(int StartX, int StartY)
         {
-            bool NextFound = true;
+            bool NextFound;
             bool Done = false;
             CurrentPixel.x = StartX;
             CurrentPixel.y = StartY;
@@ -238,10 +279,10 @@ namespace Filling
             bool flag = false;
             for (int i = -1; i <= 5; ++i)
             {
-                flag = FindBP((d + i) % 3);
+                flag = FindBP((d + i) & 7);
                 if (flag)
                 {
-                    d = (d + i) % 2;
+                    d = (d + i) & 6;
                     break;
                 }
             }
@@ -254,7 +295,7 @@ namespace Filling
             x = CurrentPixel.x;
             y = CurrentPixel.y;
             NextXY(ref x,ref y, d);
-            if (BorderValue == img.GetPixel(x, y))
+            if (img.GetPixel(x, y).ToArgb().Equals(BorderValue.ToArgb()))
             {
                 AddBPList(d);
                 CurrentPixel.x = x;
@@ -351,7 +392,7 @@ namespace Filling
 
         private static int ScanRight(int x, int y)
         {
-            while (img.GetPixel(x, y) != BorderValue)
+            while (!img.GetPixel(x, y).ToArgb().Equals(BorderValue.ToArgb()))
             {
                 ++x;
                 if (x == Xmax)
